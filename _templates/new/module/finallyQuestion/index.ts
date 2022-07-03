e8238c96c96e990d352ea0355533b4fa1868a7d2
ShebangDog <twilight.sky.asterisk@gmail.com>
2022-07-03 22:03:21 +0900

    feature: add template for statelesss component

--- _templates/new/module/finallyQuestion/index.ts
+++ _templates/new/module/finallyQuestion/index.ts
@@ -2,6 +2,11 @@ import { QuestionFlow } from "../types/QuestionFlow"
 
 export const finallyQuestion: QuestionFlow = [
   {
+    type: "confirm",
+    name: "hasStatelessComponent",
+    message: "Has it Stateless Component?"
+  },
+  {
     type: "input",
     name: "componentName",
     message: "What is the name of it component?"
